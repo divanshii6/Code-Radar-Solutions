bf8eeca3cd88cc39ee4e1e2cb0d4020a32a02ef9
Divanshi Garg <divanshigarg11@gmail.com>
2025-03-28 00:06:20 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,22 +1,40 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int n,i;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(i=0;i<n;i++){
+//         scanf("%d",&a[i]);
+//     }
+//     int max=a[0];
+//     int smax=a[0];
+//     for(i=0;i<n;i++){
+//         if(max<a[i]){
+//            int smax=max;
+//            int max=a[i];
+//         }
+//         else if(smax<a[i] && max!=a[i]){
+//             smax=a[i];
+//         }
+//     }
+//     printf("%d",smax);
+// }
 #include<stdio.h>
 int main(){
-    int n,i;
-    scanf("%d",&n);
-    int a[n];
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
+   int i, j, k,max,smax;
+   int a[6]={1,2,3,4,4,2};
+   max=a[0];
+   smax=a[0];
+ for(i=0;i<=5;i++){
+    if (max<a[i]){
+        smax=max;
+        max=a[i];
     }
-    int max=a[0];
-    int smax=a[0];
-    for(i=0;i<n;i++){
-        if(max<a[i]){
-           int smax=max;
-           int max=a[i];
-        }
-        else if(smax<a[i] && max!=a[i]){
-            smax=a[i];
-        }
+    else if(smax=a[i] && max!=a[i]){
+        smax=a[i];
     }
-    printf("%d",smax);
+ }
+ printf("%d",smax);
+   return 0;
 }
