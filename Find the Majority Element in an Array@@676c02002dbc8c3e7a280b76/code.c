1cda91a00cfa24aef4b0abb1fc31efc73201a62e
Divanshi Garg <divanshigarg11@gmail.com>
2025-03-28 00:39:00 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -1,25 +1,63 @@
-// Your code here...
+// // Your code here...
+// #include<stdio.h>
+// int main(){
+//     int i,j,n,check=0;check1=-1;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(i=0;i<n;i++){
+//         scanf("%d ",&a[i]);
+//     }
+//     for(i=0;i<n;i++){
+//         int result=1;
+//         for(j=1;j<n;j++){
+//             if(a[i]==a[j] && a[i]!=a[j]){
+//                result++
+            
+//             } 
+//             if(result>check){
+//                 result=a[i];
+//             } 
+//         }
+        
+//     }
+//     if(check>(n/2)){
+//         printf("%d",c);
+//     }
+//     else{
+//         printf("%d",check1);
+//     }
+// }
 #include<stdio.h>
-int main(){
-    int i,j,n,check=0,check1=-1,c;
-    scanf("%d",&n);
+
+int main() {
+    int i, j, n, max_count = 0, result = -1;
+    scanf("%d", &n);
     int a[n];
-    for(i=0;i<n;i++){
-        scanf("%d ",&a[i]);
+    
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);  // Removed extra space
     }
-    for(i=0;i<n;i++){
-        for(j=1;j<n;j++){
-            if(a[i]==a[j]){
-               check++;
-               break;
-            }  
+    
+    for (i = 0; i < n; i++) {
+        int current_count = 1;
+        for (j = 0; j < n; j++) {
+            if (i != j && a[i] == a[j]) {
+                current_count++;
+            }
         }
-        c=i;
+        
+        if (current_count > max_count) {
+            max_count = current_count;
+            result = a[i];
+        }
     }
-    if(check>(n/2)){
-        printf("%d",c);
+    
+    if (max_count > n/2) {
+        printf("%d", result);
     }
-    else{
-        printf("%d",check1);
+    else {
+        printf("-1");
     }
+    
+    return 0;
 }
