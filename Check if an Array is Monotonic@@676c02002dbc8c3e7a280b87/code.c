42c89f77189ef6c392722b8f81094ec94e2cf61a
Divanshi Garg <divanshigarg11@gmail.com>
2025-03-29 22:53:14 +0530

    Solution to Check if an Array is Monotonic

--- Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
+++ Check if an Array is Monotonic@@676c02002dbc8c3e7a280b87/code.c
@@ -1,21 +1,46 @@
-// Your code here...
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,n,ans=0;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(i=0;i<n;i++){
+//         scanf("%d",&a[i]);
+//     }
+//     for(i=0;i<=n-1;i++){
+//         if((a[i]<=a[i+1])||(a[i]>=a[i+1])){
+//             ans=1;
+//         }
+//     }
+//     if(ans==1){
+//         printf("YES");
+//     }
+//     else{
+//         printf("NO");
+//     }
+// }
 #include<stdio.h>
 int main(){
-    int i,j,k,n,ans=0;
+    int i, n, ans=0;
     scanf("%d",&n);
     int a[n];
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-    for(i=0;i<=n-1;i++){
-        if((a[i]<=a[i+1])||(a[i]>=a[i+1])){
-            ans=1;
-        }
+    
+    // Check if the array is monotonic (all increasing or all decreasing)
+    int increasing = 1, decreasing = 1;
+    
+    for(i=0; i<n-1; i++){
+        if(a[i] > a[i+1]) increasing = 0;
+        if(a[i] < a[i+1]) decreasing = 0;
     }
-    if(ans==1){
+    
+    if(increasing || decreasing){
         printf("YES");
-    }
-    else{
+    } else{
         printf("NO");
     }
+    
+    return 0;
 }
